02d0695d76468bb593a143913a9a29b5e43e5b26
PrajwolM <50495631+PrajwolM@users.noreply.github.com>
2020-01-30 10:06:47 +0545

    asdf

---
+++ BST.h
@@ -0,0 +1,12 @@
+#ifndef BINARYTREE_h
+#define BINARYTREE_h
+
+
+class BST
+{
+	public:
+		virtual void add (int data)=0;
+		virtual void preorderTaversal()=0;
+		void bool search(int data)=0;
+};
+#endif
