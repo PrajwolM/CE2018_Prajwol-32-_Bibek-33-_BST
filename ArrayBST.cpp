9963242935c47c8e8aad31010e5883dda8886038
PrajwolM <50495631+PrajwolM@users.noreply.github.com>
2020-01-30 11:03:12 +0545

    new

---
+++ ArrayBST.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "ArrayBST.h"
+using namespace std;
+ArrayBST::ArrayBST(){
+	for(int i=0;i<MAX-SIZE;i++){
+		this ->data[i]=0;
+	}
+}
+void ArrayBST::add(int data){
+	
+}
+void ArrayBST::preorderTaversal(){
+	
+}
+bool ArrayBST::search(int data){
+
+}
+
