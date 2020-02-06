42ff51962a0f3c8c0295bbb0eb05ad67a0d6a373
PrajwolM <50495631+PrajwolM@users.noreply.github.com>
2020-02-06 07:18:44 +0545

    1

--- ArrayBST.cpp
+++ ArrayBST.cpp
@@ -7,12 +7,44 @@ ArrayBST::ArrayBST(){
 	}
 }
 void ArrayBST::add(int data){
+	if(this->data[0]==0){
+		data[1]=data;
+	}
+	else{
+		for(int i=1;i<=MAXSIZE; ){
+			if(data<this->data[i]){
+				i=2*i;
+			}
+			else{
+				i=2*i+1;
+			}
+			if(this->data[i]==0){
+				this->data[i]=data;
+				break;
+			}
+		}
+	}
+	this->data[0]=this->data[0]+1;
 	
 }
 void ArrayBST::preorderTaversal(){
 	
 }
 bool ArrayBST::search(int data){
+	int current_index=1;
+	while(current_index<=MAXSIZE){
+	    if(data[current_index]==data){
+	        return true;
+	        break;
+	    }
+	    else if(data[current_index]>data){
+	        current_index=2*current_index;
+	    }
+	    else if(data[current_index]<data){
+	        current_index=2*current_index+1;
+	    }
+	}
+	return false;
 
 }
 
